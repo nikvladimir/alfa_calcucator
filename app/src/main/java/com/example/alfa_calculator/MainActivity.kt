adc095ada06a8b7ac541738349f7651119ed25f1
nikvladimir <alfa.voland@mail.ru>
2022-10-23 17:45:10 +0600

    added buttons

--- app/src/main/java/com/example/alfa_calculator/MainActivity.kt
+++ app/src/main/java/com/example/alfa_calculator/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.alfa_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun setTextField(str: String) {
+        entered_text.append()
     }
 }
