b122dde42defa32e8ddcea845ccfb59f9b0fed89
nikvladimir <alfa.voland@mail.ru>
2022-10-25 23:39:43 +0600

    added some actions

--- app/src/main/java/com/example/alfa_calculator/MainActivity.kt
+++ app/src/main/java/com/example/alfa_calculator/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.alfa_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import com.example.alfa_calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +12,55 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btn0.setOnClickListener { actionForViewInput("0") }
+        binding.btn1.setOnClickListener { actionForViewInput("1") }
+        binding.btn2.setOnClickListener { actionForViewInput("2") }
+        binding.btn3.setOnClickListener { actionForViewInput("3") }
+        binding.btn4.setOnClickListener { actionForViewInput("4") }
+        binding.btn5.setOnClickListener { actionForViewInput("5") }
+        binding.btn6.setOnClickListener { actionForViewInput("6") }
+        binding.btn7.setOnClickListener { actionForViewInput("7") }
+        binding.btn8.setOnClickListener { actionForViewInput("8") }
+        binding.btn9.setOnClickListener { actionForViewInput("9") }
+        binding.btnDot.setOnClickListener { actionForViewInput(",") }
+
+        binding.btnOpenBracket.setOnClickListener { actionForViewInput("(") }
+        binding.btnCloseBracket.setOnClickListener { actionForViewInput(")") }
+        binding.btnDivide.setOnClickListener { actionForViewInput("/") }
+        binding.btnMultiply.setOnClickListener { actionForViewInput("*") }
+        binding.btnMinus.setOnClickListener { actionForViewInput("-") }
+        binding.btnPlus.setOnClickListener { actionForViewInput("+") }
+
+        binding.btnHistory.setOnClickListener { showHistory() }
+        binding.btnPlusMinus.setOnClickListener { changeSing() }
+        binding.btnEqual.setOnClickListener { countResult() }
+        binding.btnDelete.setOnClickListener { dropLast() }
+        binding.btnClear.setOnClickListener {
+            binding.textViewInput.text = ""
+            binding.textViewResult.text = ""
+        }
+
     }
 
-    fun actionForViewInput(str: String) {
+    private fun actionForViewInput(str: String) {
         binding.textViewInput.append(str)
     }
 
+    private fun dropLast() {
+        binding.textViewInput.text = binding.textViewInput.text.dropLast(1)
+    }
+
+    private fun countResult() {
+        if (binding.textViewInput.text == "")
+            binding.textViewResult.text = "you put nothing"
+        else binding.textViewResult.text = "will be answer"
+    }
+
+    private fun changeSing() {
+        binding.textViewResult.text = "I don`t know how to do it"
+    }
+
+    private fun showHistory() {
+        binding.textViewResult.text = "I don`t know what to do"
+    }
 }
